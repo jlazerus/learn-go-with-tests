168ff2dc31fcae9d4d8deb31058d99ad99dc0e00
Lazerus, Jeffrey <jeffrey.lazerus@homeadvisor.com>
2020-04-12 16:38:40 -0600

    Test passes

--- 04_iteration/iteration_test.go
+++ 04_iteration/iteration_test.go
@@ -1,12 +1,28 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeatCount := 10
+	repeated := Repeat("a", repeatCount)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", repeated, expected)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 500)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("K", 5)
+	fmt.Println(repeat)
+	// Output: KKKKK
 }
