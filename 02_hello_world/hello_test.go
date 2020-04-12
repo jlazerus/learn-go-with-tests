2897d928be339f9b1c2fbe21f6238b290cad51f2
Lazerus, Jeffrey <jeffrey.lazerus@homeadvisor.com>
2020-04-12 15:31:07 -0600

    Init adder and test

--- 02_hello_world/hello_test.go
+++ 02_hello_world/hello_test.go
@@ -11,14 +11,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jeff")
+		got := Hello("Jeff", "")
 		want := "Hello, Jeff"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Franch", func(t *testing.T) {
+		got := Hello("Jéff", "french")
+		want := "Bonjour, Jéff"
 
 		assertCorrectMessage(t, got, want)
 	})
