a8d6c8c7e5839778690e90ca9aa4ba89944bcefa
Lazerus, Jeffrey <jeffrey.lazerus@homeadvisor.com>
2020-04-19 15:33:32 -0600

    Write tests for some string lib exploration

--- 04_iteration/iteration.go
+++ 04_iteration/iteration.go
@@ -1,10 +1,21 @@
 package iteration
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
-
 	return repeated
+}
+
+func MyTrimFunc(s string) string {
+	t := strings.TrimFunc(s, func(r rune) bool {
+		return unicode.IsLetter(r)
+	})
+	return t
 }
--- 04_iteration/iteration_test.go
+++ 04_iteration/iteration_test.go
@@ -19,10 +19,21 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 500)
 	}
+	println(b.N)
 }
 
 func ExampleRepeat() {
 	repeat := Repeat("K", 5)
 	fmt.Println(repeat)
 	// Output: KKKKK
+}
+
+func TestMyTrimFunc(t *testing.T) {
+	trimmed := MyTrimFunc("¡¡¡Hello, Gophers!!!")
+	expected := "Hello, Gophers"
+
+	if trimmed != expected {
+		t.Errorf("expected %q but got %q", trimmed, expected)
+	}
+
 }
