6cab6c703f715a0d7ca388e7d266f949e75239b0
Lazerus, Jeffrey <jeffrey.lazerus@homeadvisor.com>
2020-05-31 17:14:43 -0600

    adds triangle

--- 06_structs_methods_and_interfaces/shapes.go
+++ 06_structs_methods_and_interfaces/shapes.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -22,6 +27,11 @@ func (r Rectangle) Area() float64 {
 func (c Circle) Area() float64 {
 	return math.Pi * (math.Pow(c.Radius, 2))
 }
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
--- 06_structs_methods_and_interfaces/shapes_test.go
+++ 06_structs_methods_and_interfaces/shapes_test.go
@@ -13,23 +13,40 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2g want %.2g", got, want)
-		}
+	// areaTests uses a Table Test, useful for testing
+	// multipe implementations of an interface
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2g want %.2g", got, tt.want)
+		}
+	}
+	// These test the each implementation of the Shape interface independently
+	//checkArea := func(t *testing.T, shape Shape, want float64) {
+	//	t.Helper()
+	//	got := shape.Area()
+	//	if got != want {
+	//		t.Errorf("got %.2g want %.2g", got, want)
+	//	}
+	//}
+	//
+	//t.Run("rectangles", func(t *testing.T) {
+	//	rectangle := Rectangle{12, 6}
+	//	checkArea(t, rectangle, 72.0)
+	//})
+	//
+	//t.Run("circles", func(t *testing.T) {
+	//	circle := Circle{10}
+	//	checkArea(t, circle, 314.1592653589793)
+	//})
 
 }
